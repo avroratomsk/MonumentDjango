d442e9d77c19f0e14221fcf9ff04aaea05cb4f09
avroratomsk <avroratomsk70@yandex.ru>
2026-03-25 11:36:09 +0300

    fix

--- main/home/urls.py
+++ main/home/urls.py
@@ -6,12 +6,14 @@ urlpatterns = [
     path('contact-form/', views.order_form, name="order_form"),
     path('callback-form/', views.callback_form, name="callback_form"),
     path('privacy/', views.privacy, name="privacy"),
+    path('cookie/', views.cookie, name="cookie"),
     path('about/', views.about, name="about"),
     path('contact/', views.contact, name="contact"),
     path('documents/', views.documents, name="documents"),
+
     path('gallery/', views.gallery, name="gallery"),
     path('<slug:slug>/', views.gallery_detail, name="gallery_detail"),
-    path('cookie/', views.cookie, name="cookie"),
+
 
     path('robots.txt', views.robots_txt),
 
